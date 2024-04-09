2f7455ef31b5266cd264bdc729b43b7467151b6b
root <root@DKR-final.ip-ptr.tech>
2024-04-09 06:23:20 +0200

    08.04.2024

--- results/telemetry_settings.php
+++ results/telemetry_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
+$db_type="postgresql"; //Type of db: "mysql", "sqlite" or "postgresql"
 $stats_password="PASSWORD"; //password to login to stats.php. Change this!!!
 $enable_id_obfuscation=false; //if set to true, test IDs will be obfuscated to prevent users from guessing URLs of other tests
 $redact_ip_addresses=false; //if set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log
@@ -23,4 +23,3 @@ $PostgreSql_databasename="mydb";
 
 //IMPORTANT: DO NOT ADD ANYTHING BELOW THIS PHP CLOSING TAG, NOT EVEN EMPTY LINES!
 ?>
-
